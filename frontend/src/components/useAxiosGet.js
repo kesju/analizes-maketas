fcc16c365c1114c53db98d12b7bae1df8882af71
kesju <kesju@live.com>
2023-03-22 13:02:22 +0200

    pataisiau useAxiosGet

--- frontend/src/components/useAxiosGet.js
+++ frontend/src/components/useAxiosGet.js
@@ -2,7 +2,7 @@ import {useEffect, useState} from 'react';
 import axios from "axios";
 
 
-const useAxiosPost = (url, param) => {
+const useAxiosGet = (url, param) => {
     const [data, setData] = useState(null);
     const [error, setError] = useState("");
     const [loaded, setLoaded] = useState(false);
@@ -27,4 +27,4 @@ const useAxiosPost = (url, param) => {
     return { data, error, loaded };
   };
 
-export default useAxiosPost;  
+export default useAxiosGet;  
